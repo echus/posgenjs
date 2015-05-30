f20a21602084e2a3d64e0d5c9e64ea9d74f3b254
Varvara Efremova <varvara@echus.co>
2015-05-30 05:12:29 +0000

    Working FCC plot

--- posgen.js
+++ posgen.js
@@ -1,18 +1,196 @@
 var posgen = {
  
     onReady: function() {
-        // Initialise chart to global var
-        // var posgen.plot = posgen.plotSetup("results");
-        posgen.plotSetup("results");
+        // Display empty chart on initialisation
+        posgen.plot = posgen.plotSetup("plot",
+                                       "Lattice", 
+                                       "No data to show yet",
+                                       [1, 1, 1],
+                                       false
+                                       );
 
-        $("#plotButton").on("click", posgen.updatePlot);
+        $("#plotButton").on("click", posgen.run);
     },
 
-    plotSetup: function(div) {
-        // Set up the chart
+    run: function(event) {
+        // Main function, do everything!
+
+        // Parse form into request json for posgen api
+        request = posgen.paramsToJSON(posgen.parseFail);
+        console.log("Parsed request json:");
+        console.log(request);
+
+        // Call posgen with parsed request
+        posgen.posgenCall(request, 
+                          posgen.plotUpdate, 
+                          posgen.backendFail
+                         );
+    },
+
+
+
+    //
+    // Function functions
+    // 
+    plotUpdate: function(points, request) {
+        console.log("plotUpdate: Received returned points");
+        console.log(points);
+        
+        // Hack to deal with Highcharts' lack of a real updateable zAxis. 
+        // To update z axis size must destroy chart and recreate appropriately 
+        // sized one. 
+        //
+        // For speed, data initialisation should really be done in the chart
+        // constructor, but ain't no one got time for that.
+        posgen.plot.destroy();
+        posgen.plot = posgen.plotSetup("plot",
+                                       "Lattice", 
+                                       "(click and drag to rotate view)",
+                                       [request.bounds.x, 
+                                        request.bounds.y, 
+                                        request.bounds.z],
+                                       true
+                                       );
+
+        // Loop through returned points by mass (element ID)
+        for (var mass in points) {
+            if (points.hasOwnProperty(mass)) {
+                // Plot each element as separate series
+                posgen.plot.addSeries({
+                    name: mass,
+                    data: points[mass]
+                });
+            }
+        }
+
+        posgen.plot.setTitle({text: "Lattice"}, {text: "(click and drag to rotate view)"});
+    },
+
+    posgenCall: function(request, onDone, onFail) {
+        // Generate points as per request params
+        //
+        // Input:
+        // ------
+        // request: JAppropriately structured request object for posgen/api
+        // done: callback on success, passed returned data and request object
+        // fail: callback on failure, passed returned data and request object
+        
+        var jqxhr = $.ajax({
+            url: "api/points",
+            type: "POST",
+            data: JSON.stringify(request),
+            contentType: "application/json; charset=utf-8",
+            dataType: "json"
+        });
+
+        jqxhr.done(function(data) {
+            onDone(data, request);
+        });
+
+        jqxhr.fail(function(data) {
+            onFail(data, request);
+        });
+    },
+
+    paramsToJSON: function(onFail) {
+        // Parses form to appropriate request object for passing to posgen backend
+        // Displays error if form incomplete or outside value bounds
+        // 
+        // Input:
+        // ------
+        // fail: Parse failure callback
+        //
+        // Returns:
+        // --------
+        // Success: Parsed form contents in posgen/api object form 
+        //          (NOT stringified)
+        // Error:   undefined
+
+        // TODO: var $error = $("#formError");
+
+        var $paramsLattice = $("#paramsLattice :input");
+        var $paramsSpacing = $("#paramsSpacing :input");
+        var $paramsBounds = $("#paramsBounds :input");
+        var $paramsMass = $("#paramsMass :input");
+
+        var lattice = {};
+        $paramsLattice.serializeArray().map(function(x){lattice[x.name] = x.value;}); 
+        var spacing = {};
+        $paramsSpacing.serializeArray().map(function(x){spacing[x.name] = x.value;}); 
+        var bounds = {};
+        bounds.bounds = {};
+        $paramsBounds.serializeArray().map(function(x){bounds.bounds[x.name] = x.value;}); 
+        var mass = {};
+        mass.mass = {};
+        $paramsMass.serializeArray().map(function(x){mass.mass[x.name] = x.value;});
+        
+        params = $.extend({}, lattice, spacing, bounds, mass);
+
+        // Ensure all required fields are filled
+        if (posgen.paramsCheck(params) === false) {
+            onFail();
+            return undefined;
+        } else {
+            return params;
+        }
+    },
+
+
+
+    // 
+    // Failure functions
+    //
+    parseFail: function() {
+        // Form parsing failure
+        console.log("ERROR: Failed to parse form data, check your input")
+    },
+
+    backendFail: function(data, request) {
+        // Backend failure
+        console.log("ERROR: Backend error")
+    },
+
+
+    // 
+    // Convenience functions
+    //
+    paramsCheck: function(params) {
+        // Check all required request elements are present and corrent
+        // 
+        // Input:
+        // ------
+        // params: Object representation of posgenapi json request to be sent
+        //
+        // Returns:
+        // --------
+        // (success) true
+        // (fail)    false
+        return true;
+    },
+
+    
+
+    //
+    // Initialisation functions
+    // 
+    plotSetup: function(container, title, subtitle, bounds, legend) {
+        // Initialises plot in the supplied container
+        //
+        // Arguments:
+        // ----------
+        // container: HTML container to render chart to
+        // title    : Chart title
+        // subtitle : Chart subtitle
+        // bounds   : Chart maximum bounds array [x, y, z]
+        // legend   : (boolean) Enable/disable legend
+        //
+        // Returns:
+        // --------
+        // chart    : Highcharts chart object
+
         var chart = new Highcharts.Chart({
             chart: {
-                renderTo: 'results',
+                renderTo: container,
                 margin: 100,
                 type: 'scatter',
                 options3d: {
@@ -30,40 +208,35 @@ var posgen = {
                 }
             },
             title: {
-                text: 'Generated lattice'
+                text: title
             },
             subtitle: {
-                text: '(click and drag to rotate)'
+                text: subtitle
             },
             plotOptions: {
                 scatter: {
-                    width: 10,
-                    height: 10,
-                    depth: 10
+                    width: bounds[0],
+                    height: bounds[1],
+                    depth: bounds[2]
                 }
             },
             yAxis: {
                 min: 0,
-                max: 10,
+                max: bounds[1],
                 title: null
             },
             xAxis: {
                 min: 0,
-                max: 10,
+                max: bounds[0],
                 gridLineWidth: 1
             },
             zAxis: {
                 min: 0,
-                max: 10
+                max: bounds[2]
             },
             legend: {
-                enabled: true
+                enabled: legend
             },
-            series: [{
-                name: 'Test data',
-                colorByPoint: true,
-                data: [[1, 6, 5], [8, 7, 9], [1, 3, 4], [4, 6, 8], [5, 7, 7], [6, 9, 6], [7, 0, 5], [2, 3, 3], [3, 9, 8], [3, 6, 5], [4, 9, 4], [2, 3, 3], [6, 9, 9], [0, 7, 0], [7, 7, 9], [7, 2, 9], [0, 6, 2], [4, 6, 7], [3, 7, 7], [0, 1, 7], [2, 8, 6], [2, 3, 7], [6, 4, 8], [3, 5, 9], [7, 9, 5], [3, 1, 7], [4, 4, 2], [3, 6, 2], [3, 1, 6], [6, 8, 5], [6, 6, 7], [4, 1, 1], [7, 2, 7], [7, 7, 0], [8, 8, 9], [9, 4, 1], [8, 3, 4], [9, 8, 9], [3, 5, 3], [0, 2, 4], [6, 0, 2], [2, 1, 3], [5, 8, 9], [2, 1, 1], [9, 7, 6], [3, 0, 2], [9, 9, 0], [3, 4, 8], [2, 6, 1], [8, 9, 2], [7, 6, 5], [6, 3, 1], [9, 3, 1], [8, 9, 3], [9, 1, 0], [3, 8, 7], [8, 0, 0], [4, 9, 7], [8, 6, 2], [4, 3, 0], [2, 3, 5], [9, 1, 4], [1, 1, 4], [6, 0, 2], [6, 1, 6], [3, 8, 8], [8, 8, 7], [5, 5, 0], [3, 9, 6], [5, 4, 3], [6, 8, 3], [0, 1, 5], [6, 7, 3], [8, 3, 2], [3, 8, 3], [2, 1, 6], [4, 6, 7], [8, 9, 9], [5, 4, 2], [6, 1, 3], [6, 9, 5], [4, 8, 2], [9, 7, 4], [5, 4, 2], [9, 6, 1], [2, 7, 3], [4, 5, 4], [6, 8, 1], [3, 4, 0], [2, 2, 6], [5, 1, 2], [9, 9, 7], [6, 9, 9], [8, 4, 3], [4, 1, 7], [6, 2, 5], [0, 4, 9], [3, 5, 9], [6, 9, 1], [1, 9, 2]]
-            }]
         });
 
         // Add mouse events for rotation
@@ -97,110 +270,10 @@ var posgen = {
                 }
             });
         });
-    },
- 
-    updatePlot: function(event) {
-        // Update plot
-        //
-        // paramsToJSON - get params and pass to getPoints
-        // getPoints w/ json from above, return array of points
-        // plotPoints w/ returned data from getpoints
 
-        console.log("Processing form input -> json");
-        request = posgen.paramsToJSON();
-        console.log("Request json:");
-        console.log(request);
-        points = posgen.getPoints(request);
-    },
-
-    getPoints: function(request) {
-        // Generate points as per request params
-        //
-        // Input:
-        // ------
-        // request: JSON with appropriately structured request for posgen/api
-        //
-        // Returns:
-        // --------
-        // points: Object { x:[], y:[], z:[], mass:[] }
-        // undefined if request fails
-        
-        var points = {};
-
-        var jqxhr = $.ajax({
-            url: "api/points",
-            type: "POST",
-            data: request,
-            contentType: "application/json; charset=utf-8",
-            dataType: "json"
-        });
-
-        jqxhr.done(function(data) {
-            console.log("api/points call success");
-            console.log(data);
-            points = data;
-        });
-
-        jqxhr.fail(function(data) {
-            console.log("api/points call fail");
-            console.log(data);
-            points = undefined;
-        });
-    },
-
-    paramsToJSON: function() {
-        // Parses form to appropriate request object for passing to posgen backend
-        // Displays error if form incomplete or outside value bounds
-        // 
-        // Returns:
-        // --------
-        // Success: stringified JSON
-        // Error:   undefined
-
-        // TODO: var $error = $("#formError");
-
-        var $paramsLattice = $("#paramsLattice :input");
-        var $paramsSpacing = $("#paramsSpacing :input");
-        var $paramsBounds = $("#paramsBounds :input");
-        var $paramsMass = $("#paramsMass :input");
-
-        var lattice = {};
-        $paramsLattice.serializeArray().map(function(x){lattice[x.name] = x.value;}); 
-        var spacing = {};
-        $paramsSpacing.serializeArray().map(function(x){spacing[x.name] = x.value;}); 
-        var bounds = {};
-        bounds.bounds = {};
-        $paramsBounds.serializeArray().map(function(x){bounds.bounds[x.name] = x.value;}); 
-        var mass = {};
-        mass.mass = {};
-        $paramsMass.serializeArray().map(function(x){mass.mass[x.name] = x.value;});
-        
-        params = $.extend({}, lattice, spacing, bounds, mass);
-
-        // Ensure all required fields are filled
-        if (posgen.paramsCheck(params) === false) {
-            // TODO: display error
-            // Handle this in updatePlot instead??
-            return undefined;
-        } else {
-            return JSON.stringify(params);
-        }
-    },
-
-    paramsCheck: function(params) {
-        // Check all required request elements are present and corrent
-        // 
-        // Input:
-        // ------
-        // params: Object representation of posgenapi json request to be sent
-        //
-        // Returns:
-        // --------
-        // (success) true
-        // (fail)    false
-        return true;
+        return chart;
     }
- 
+
 };
  
 $(document).ready(posgen.onReady);
